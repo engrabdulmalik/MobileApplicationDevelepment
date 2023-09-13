745ebdbd75f40abf01142758711c0c812166c93c
engrabdulmalik <engrabdulmaliq@gmail.com>
2023-09-13 09:55:47 +0500

    wrapper component

--- expensecalculator/src/App.js
+++ expensecalculator/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 const expenses = [
   { title: "Car Insurance", amount: 6000, date: new Date(2023, 9, 13) },
   { title: "Car Navigation", amount: 8000, date: new Date(2023, 9, 12) },
@@ -9,26 +9,7 @@ const expenses = [
 function App() {
   return (
     <div>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
