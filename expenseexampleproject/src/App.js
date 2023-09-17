6bb0102186a495d5738b636241e64dad85a883da
engrabdulmalik <engrabdulmaliq@gmail.com>
2023-09-17 07:21:34 +0500

    new

---
+++ expenseexampleproject/src/App.js
@@ -0,0 +1,17 @@
+import Expenses from "./components/Expenses";
+
+function App() {
+  const expenses = [
+    { title: "Car Insurance", amount: 6000, date: new Date(2023, 9, 13) },
+    { title: "Car Seat", amount: 8000, date: new Date(2023, 9, 12) },
+    { title: "Car Tyres", amount: 9000, date: new Date(2023, 9, 11) },
+    { title: "Car Lights", amount: 10000, date: new Date(2023, 9, 10) },
+  ];
+  return (
+    <div>
+     <Expenses items = {expenses}></Expenses>
+    </div>
+  );
+}
+
+export default App;
